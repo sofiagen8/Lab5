b44d3a81a132a0bbad91bdf7253b4368e1279c3b
sofiagen8 <c26sofia.gentile@afacademy.af.edu>
2026-04-29 10:18:26 -0600

    added FSM

--- src/controller_fsm.vhd
+++ src/controller_fsm.vhd
@@ -38,8 +38,42 @@ entity controller_fsm is
 end controller_fsm;
 
 architecture FSM of controller_fsm is
+    type sm_state is (s_0, s_1, s_2, s_3);
+	
+	signal f_Q, f_Q_next: sm_state;
 
 begin
 
+	-- CONCURRENT STATEMENTS ------------------------------------------------------------------------------
+	
+	-- Next State Logic
+    f_Q_next <= s_0 when (f_Q = s_3 and i_adv = '1') else -- going up
+            s_0 when (i_reset = '1') else
+            s_1 when (f_Q = s_0 and i_adv = '1') else
+            s_2 when (f_Q = s_1 and i_adv = '1') else --when top floor and it will stay there while 1
+            s_3 when (f_Q = s_2 and i_adv = '0') else -- going down
+            s_0; -- default case 
+            
+	-- Output logic
+    with f_Q select
+    o_cycle <= "0001" when s_0,
+            "0010" when s_1,
+            "0011" when s_2,
+            "0100" when s_3,
+            "0001" when others; -- default is s_0
+
+	-------------------------------------------------------------------------------------------------------
+	
+	-- PROCESSES ------------------------------------------------------------------------------------------	
+	--IF SOMETHING NOT WORKING CORRECTLY, THEN REGISTER HERE IS AT FAULT FOR FSM
+	-- State register ------------
+	register_proc : process (i_adv, i_reset)
+    begin
+        if i_reset = '1' then
+            f_Q <= s_0; 
+        elsif (rising_edge(i_adv)) then
+            f_Q <= f_Q_next; --next state becomes current state
+        end if;
+    end process register_proc;
 
 end FSM;
